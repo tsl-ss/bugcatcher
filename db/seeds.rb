ccc08d520617c9bfcd842fc1ef231132c7fdf203
Wade Novak <wade.novak@gmail.com>
2015-03-10 11:54:27 -0500

    Adds seed data for users and projects

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,67 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+user_info = [
+{
+  :email => "user1@example.com",
+  :password => "topsecret",
+},
+{
+  :email => "user2@example.com",
+  :password => "topsecret",
+},
+{
+  :email => "user3@example.com",
+  :password => "topsecret",
+}
+]
+
+user_info.each do |user|
+  u = User.new
+  u.email = user[:email]
+  u.password = user[:password]
+  u.save
+end
+
+puts "There are now #{User.count} users in the database"
+
+project_info = [
+{
+  :name => "A-Lister",
+  :description => "Right app, right time",
+  :url => "http://www.google.com",
+  :user_id => "1"
+},
+{
+  :name => "D-League",
+  :description => "Play basketball with anyone in the city at any time",
+  :url => "http://www.starterleague.com",
+  :user_id => "2"
+},
+{
+  :name => "Cardstock",
+  :description => "Connecting teachers with parents to better student experience",
+  :url => "http://www.nba.com",
+  :user_id => "3"
+},
+{
+  :name => "Crowdgate",
+  :description => "Uber for Tailgating",
+  :url => "http://www.nfl.com",
+  :user_id => "1"
+},
+{
+  :name => "Streamline",
+  :description => "Helps you list equipment in more places in less time",
+  :url => "http://www.yahoo.com",
+  :user_id => "3"
+}
+]
+
+project_info.each do |project|
+  p = Project.new
+  p.name = project[:name]
+  p.description = project[:description]
+  p.url = project[:url]
+  p.user_id = project[:user_id]
+  p.save
+end
+
+puts "There are now #{Project.count} projects in the database"
