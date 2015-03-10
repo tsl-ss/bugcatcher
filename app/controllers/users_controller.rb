c0efdafe586ac661c3997c5415bd5f180a79a7d5
Alex Bahn <AanderBahn@gmail.com>
2015-03-10 14:15:39 -0500

    Add avatar presence validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,6 +48,7 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
+
     @user.destroy
     respond_to do |format|
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
