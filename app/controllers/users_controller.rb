2f54b951985eeb0d7ae3ba3cf20f8a9928d61760
Alex Bahn <AanderBahn@gmail.com>
2015-03-10 14:13:14 -0500

    Add :avatar param to Users permitted params in Users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -63,6 +63,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :bio, :email, :github, :linkedin, :twitter, :facebook)
+      params.require(:user).permit(:name, :bio, :email, :github, :linkedin, :twitter, :facebook, :avatar)
     end
 end
