42055c8c7e46bd31aa0643efea8ba591bfca05c9
Lanny Bose <landric@gmail.com>
2015-03-10 11:30:18 -0500

    Fixing project belongs_to owner with user_id table column.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
-  belongs_to :owner, class_name: "User"
+  belongs_to :owner, class_name: "User", foreign_key: "user_id"
   has_many :screenshots
 
   has_attached_file :logo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
