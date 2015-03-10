90061baa8f64f21e469d7cf22999f747aa430ab0
Lanny Bose <landric@gmail.com>
2015-03-10 11:07:15 -0500

    Added associations b/t User, Project, Screenshot and validations for Project and Screenshot

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,11 @@
 class Project < ActiveRecord::Base
+  belongs_to :owner, class_name: "User"
+  has_many :screenshots
+
+  has_attached_file :logo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :logo, :content_type => /\Aimage\/.*\Z/
+
+  validates :name, presence: true, uniqueness: { scope: :user }
+  validates :description, presence: true
+
 end
