236993108f7838216cb574a05703f76e00c2f42e
Lanny Bose <landric@gmail.com>
2015-03-12 15:28:34 -0500

    Refactored the Project show to include partials for releases. Routing for close release links.

--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -8,6 +8,14 @@ class ReleasesController < ApplicationController
   def show
   end
 
+  def close
+    #replace after merging with the set release before_action
+    @release = Release.find(params[:id])
+    @release.close_release
+    @release.save
+    redirect_to :back
+  end
+
   def delete
   end
 end
