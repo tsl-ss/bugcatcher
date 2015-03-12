dd3b7b44b5323ebfec152ca7e5975c4e5f82b348
Daniel Lopes <danielvlopes@gmail.com>
2015-03-12 11:48:43 -0500

    small markup changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150310162225) do
+ActiveRecord::Schema.define(version: 20150310190712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,10 +42,32 @@ ActiveRecord::Schema.define(version: 20150310162225) do
   add_index "screenshots", ["project_id"], name: "index_screenshots_on_project_id", using: :btree
 
   create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.text     "bio"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "github"
+    t.string   "linkedin"
+    t.string   "twitter"
+    t.string   "facebook"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "projects", "users"
   add_foreign_key "screenshots", "projects"
