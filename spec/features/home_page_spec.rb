c5a71f6002dff6fe111957a058d2f98dfd8d6af2
Marcus Derencius <derencius@gmail.com>
2015-03-12 11:53:06 -0500

    updating the view home page integration test

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,16 +1,8 @@
 require 'rails_helper'
+
 feature "Home Page" do
-  background do
-
-  end
-
   scenario "viewing it" do
-    visit '/'
-    within(".projects") do
-      fill_in 'Email', :with => 'user@example.com'
-      fill_in 'Password', :with => 'caplin'
-    end
-
-    expect(page.find('.projects')).to have_content 'Project #1'
+    visit "/"
+    expect(page).to have_content 'Bug Catcher'
   end
 end
