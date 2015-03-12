0ab7351c152502c929ef4fb353bc095685a0db28
Chris Moy <moyboy530@gmail.com>
2015-03-12 14:40:57 -0500

    make nested resource

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   resource :profile, except: [:new, :create]
-  resources :projects
-  resources :releases
+  resources :projects do
+    resources :releases
+  end
 end
