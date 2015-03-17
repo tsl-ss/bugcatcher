07fd88d2d9afb947b5c5cf0c2bc7e9d9d347f20c
Wade Novak <wade.novak@gmail.com>
2015-03-17 10:19:09 -0500

    Adds devise controllers and gets successful sign in and sign up routed

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root    'static_page#home'
   get     'static_page/home'
 
-  devise_for :users
+  devise_for :users, controllers: { sessions: "users/sessions", registrations: "users/registrations" }
   resource :profile, except: [:new, :create]
   resources :projects
 end
