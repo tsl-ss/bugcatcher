ef356903a94bf969b269fb390b5f3e689fc4d1c0
Marcus Derencius <derencius@gmail.com>
2015-03-17 10:34:48 -0500

    Flash message check into integration testing

--- test/integration/user_flows_test.rb
+++ test/integration/user_flows_test.rb
@@ -5,7 +5,7 @@ class UserFlowsTest < ActionDispatch::IntegrationTest
     get "/users/sign_up"
     post_via_redirect "/users", user: {email: 'user@email.com', password: '12345678', password_confirmation: '12345678'}
     assert_equal '/', path
-
+    assert_equal 'Welcome! You have signed up successfully.', flash[:notice]
     get "/projects"
     assert_response :success
     assert assigns(:projects)
