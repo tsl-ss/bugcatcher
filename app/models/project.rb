1e53fdbc4ff194430c0c8322f34c4193b3d2ebd9
Daniel Lopes <danielvlopes@gmail.com>
2015-03-17 11:27:09 -0500

    formating changes

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,12 +3,13 @@ class Project < ActiveRecord::Base
   has_many :screenshots, as: :screenshotable, dependent: :destroy
   has_many :releases
 
-  has_attached_file :logo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "default_bug_logo.png"
+  has_attached_file :logo,
+    :styles => { :medium => "300x300>", :thumb => "100x100>" },
+    :default_url => "default_bug_logo.png"
 
   validates_attachment_content_type :logo, :content_type => /\Aimage\/.*\Z/
   validates :name, presence: true, uniqueness: { scope: :user_id }
   validates :description, presence: true
 
   accepts_nested_attributes_for :screenshots, allow_destroy: true
-
 end
