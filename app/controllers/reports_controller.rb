57f34376dbf8a801d40e01109926164e1eeb40f0
Paul Gonzalez <pjg5005@gmail.com>
2015-03-17 13:40:50 -0500

    Creates from for new report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,9 @@
 class ReportsController < ApplicationController
 
   def new
+    @project = params[:project_id]
+    @release = params[:release_id]
+    @report = Report.new
   end
 
   def show
@@ -13,6 +16,13 @@ class ReportsController < ApplicationController
   end
 
   def create
+    @report = Report.new(report_params)
+    @report.release_id = params[:release_id]
+    @report.save
+  end
+
+  def report_params
+    params.require(:report).permit(:content)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,21 @@
 Rails.application.routes.draw do
 
-  get 'reports/new'
-
-  get 'reports/show'
-
-  get 'reports/edit'
-
-  get 'reports/delete'
-
-  get 'reports/create'
-
   root    'static_page#home'
   get     'static_page/home'
 
+
   devise_for :users, controllers: { sessions: "users/sessions", registrations: "users/registrations" }
+
   resource :profile, except: [:new, :create]
+
+
   resources :projects do
     resources :releases do
       member do
         get :close
       end
+      resources :reports
     end
   end
+
 end
