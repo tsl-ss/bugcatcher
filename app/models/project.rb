ad6125dd3545854f00f3cf2d5469ab940dd635c4
Daniel Lopes <danielvlopes@gmail.com>
2015-03-17 13:24:35 -0500

    changing default logo for project

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,7 +5,7 @@ class Project < ActiveRecord::Base
 
   has_attached_file :logo,
     :styles => { :medium => "300x300>", :thumb => "100x100>" },
-    :default_url => "default_bug_logo.png"
+    :default_url => "default_logo.png"
 
   validates_attachment_content_type :logo, :content_type => /\Aimage\/.*\Z/
   validates :name, presence: true, uniqueness: { scope: :user_id }
