1bc8d41261689a3d355392f6a26e1e383cd69b62
Paul Gonzalez <pjg5005@gmail.com>
2015-03-17 12:59:45 -0500

    Creates controller for reports

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,16 @@
+class ReportsController < ApplicationController
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+
+  def create
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150317174223) do
+ActiveRecord::Schema.define(version: 20150317175806) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20150317174223) do
     t.integer  "release_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "content"
   end
 
   add_index "reports", ["release_id"], name: "index_reports_on_release_id", using: :btree
