ae1b1681fff97d0ea85dfbd5d9a7e4610953f621
Daniel Lopes <danielvlopes@gmail.com>
2015-04-02 11:54:11 -0300

    Be consistent, or we handle json requests or we don't
    Rails comes with the native support to handle json requests if you use
    scaffold. I love this feature and try to make sure it works even when I
    customize the generated code but that's something you have to do
    manually. And I don't think we gonna worry about providing an API for
    this project any time soon, and since most of the controllers are not
    handling json requests/responses properly already I'm just gonna remove
    the respond to block.
    
    In theory we are making our controller not rest
    anymore because the REST concept assumes that a resource can be accessible using many
    formats instead of just plain html, for example, another software we
    probably not be able to handle html like a browser does. So that's a
    trade-off and for now I carry more about serving html properly than
    supporting different types of accesses.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,23 +2,16 @@ class ProfilesController < ApplicationController
   before_action :authenticate_user!
 
   def update
-    respond_to do |format|
-      if current_user.update(user_params)
-        format.html { redirect_to profile_url, notice: 'Your account was updated' }
-        format.json { render :show, status: :ok, location: current_user }
-      else
-        format.html { render :edit }
-        format.json { render json: current_user.errors, status: :unprocessable_entity }
-      end
+    if current_user.update(user_params)
+      redirect_to profile_url, notice: 'Your account was updated'
+    else
+      render :edit
     end
   end
 
   def destroy
     current_user.destroy
-    respond_to do |format|
-      format.html { redirect_to profile_url, notice: 'We are sad to see you go. Your account was canceled.' }
-      format.json { head :no_content }
-    end
+    redirect_to profile_url, notice: 'We are sad to see you go. Your account was canceled.'
   end
 
   private
