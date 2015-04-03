231e54b70099ce5dfc2ad4a2a9e44956d49b5fe6
Daniel Lopes <danielvlopes@gmail.com>
2015-04-03 11:07:09 -0300

    Adding a has_one project to the report

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,6 +1,8 @@
 class Report < ActiveRecord::Base
-  belongs_to :release
   has_many :comments
+  has_one :project, through: :release
+
+  belongs_to :release
   belongs_to :author, class_name: "User", foreign_key: "author_id"
 
   scope :accepted, -> { where(accepted_by_project_owner: true) }
