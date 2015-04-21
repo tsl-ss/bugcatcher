642662784c3c0f3808c80080a3933f2c16c1ef72
Woodworth <woodworthcommunications@gmail.com>
2015-04-21 14:26:05 -0500

    Add test for report model

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def test_report_accepted_by_project_owner_scope
+    report1 = reports(:first_report)
+    report2 = reports(:second_report)
+
+    # Test for report1
+    assert_equal(Report.accepted, [report1])
+
+    # Test for report2
+    refute_equal(Report.accepted, [report2])
+  end
+
 end
