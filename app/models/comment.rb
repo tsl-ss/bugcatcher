8d08fd1a10b4f7db047f7142118da69042be045c
Paul Gonzalez <pjg5005@gmail.com>
2015-04-21 10:31:45 -0500

    Adds test coverage for creation attributes for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :report
 
+  validates_presence_of :user, :report, :text
+
   has_attached_file :attachment, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 
   validates_attachment_content_type :attachment, :content_type => /\Aimage\/.*\Z/
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -2,20 +2,37 @@ require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
 
-  def test_user_comment_count
+  def test_required_attributes_for_creation
+    new_comment = Comment.new
+
+    assert(!new_comment.valid?)
+    assert(new_comment.errors.messages[:text].present?)
+    assert(new_comment.errors.messages[:report].present?)
+    assert(new_comment.errors.messages[:user].present?)
+
     user = users(:daniel)
-    comment_count = user.comments.count
-    new_comment = user.comments.new
-    new_comment.save
-    assert_equal(comment_count + 1, user.comments.count)
+    report = reports(:first_report)
+
+    new_comment.text = "Lorem"
+    new_comment.user = user
+    new_comment.report = report
+
+    assert(new_comment.valid?)
+    new_comment.save!
   end
 
-  def test_report_comment_count
+  def test_user_and_report_comment_count
+    user = users(:daniel)
+    user_comment_count = user.comments.count
+
     report = reports(:first_report)
-    comment_count = report.comments.count
-    new_comment = report.comments.new
-    new_comment.save
-    assert_equal(comment_count + 1, report.comments.count)
+    report_comment_count = report.comments.count
+
+    new_comment = Comment.new(:text => "lorem", :user => user, :report => report)
+    new_comment.save!
+
+    assert_equal(user_comment_count + 1, user.comments.count)
+    assert_equal(report_comment_count + 1, report.comments.count)
   end
 
 end
