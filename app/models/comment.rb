6dc53bc0f1776cc6499f2570b1a77ee52db44827
Paul Gonzalez <pjg5005@gmail.com>
2015-04-21 10:20:00 -0500

    Adds test coverage for comments model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,5 +5,4 @@ class Comment < ActiveRecord::Base
   has_attached_file :attachment, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 
   validates_attachment_content_type :attachment, :content_type => /\Aimage\/.*\Z/
-
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def test_user_comment_count
+    user = users(:daniel)
+    comment_count = user.comments.count
+    new_comment = user.comments.new
+    new_comment.save
+    assert_equal(comment_count + 1, user.comments.count)
+  end
+
+  def test_report_comment_count
+    report = reports(:first_report)
+    comment_count = report.comments.count
+    new_comment = report.comments.new
+    new_comment.save
+    assert_equal(comment_count + 1, report.comments.count)
+  end
+
 end
